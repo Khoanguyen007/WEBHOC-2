4c33c9089de9cacb577644124d8e7fc3ac104bfe
Khoanguyen007 <khoanguyenankhe2004@gmail.com>
2025-12-02 20:49:49 +0700

    Delete files.js
    Signed-off-by: Khoanguyen007 <khoanguyenankhe2004@gmail.com>

--- files.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { generatePresignedUrl } = require('../controllers/fileController');
-const { authMiddleware } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.post('/upload-request', authMiddleware, generatePresignedUrl);
-
-module.exports = router;
